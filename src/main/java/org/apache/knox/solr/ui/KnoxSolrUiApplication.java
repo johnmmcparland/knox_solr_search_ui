797bd5ea18a9d2d92f0bd7133bb3853fd72d9cfc
John McParland <johnmmcparland@gmail.com>
2016-11-17 20:20:45 +0000

    Added Spring Boot Biolerplate

---
+++ src/main/java/org/apache/knox/solr/ui/KnoxSolrUiApplication.java
@@ -0,0 +1,12 @@
+package org.apache.knox.solr.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoxSolrUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoxSolrUiApplication.class, args);
+	}
+}
